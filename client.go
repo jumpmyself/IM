3cdae1603462a672602f29e887ee10e52b139c70
ZYL <gulizhu93@gmail.com>
2025-01-10 23:01:00 +0800

    【IM即时通信】-【客户端】-菜单显示

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	Conn       net.Conn
+	flag       int // 当前client模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       99,
 	}
 	// 连接server
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
@@ -28,6 +30,51 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client.Conn = conn
 	// 返回对象
 	return client
+}
+
+// 菜单
+func (client *Client) Menu() bool {
+	var (
+		flag int
+	)
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名模式")
+	fmt.Println("0.推出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>>>>>> 请输出合法范围内的数字（1，2，3，4）>>>>>>>>>>")
+		return false
+	}
+}
+
+// 客户端run方法
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.Menu() != true {
+		}
+		// 根据不同的模式处理不同的业务
+		switch client.flag {
+		case 1:
+			// 公聊模式
+			fmt.Println("公聊模式选择...")
+			break
+		case 2:
+			// 私聊模式
+			fmt.Println("私聊模式选择...")
+			break
+		case 3:
+			// 更新用户名
+			fmt.Println("更新用户名选择...")
+			break
+		}
+	}
+
 }
 
 var (
@@ -53,6 +100,6 @@ func main() {
 	fmt.Println(">>>>>>>>> 连接服务器成功 >>>>>>>>>>")
 
 	// 启动客户端的业务
-	select {}
+	client.Run()
 
 }
