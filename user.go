367001485bcc3fe1233101fcc402b9f0f779f459
ZYL <gulizhu93@gmail.com>
2025-01-10 22:02:15 +0800

    【IM即时通信】-在线用户查询

--- user.go
+++ user.go
@@ -63,7 +63,24 @@ func (this *User) Offline() {
 	this.server.Broadcast(this, "下线")
 }
 
+// 给当前User对应的客户端发送消息
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg + "\n"))
+
+}
+
 // 用户处理消息的业务
 func (this *User) DoMessage(msg string) {
-	this.server.Broadcast(this, msg)
+	if msg == "who" {
+		// 查询当前在线用户都有哪些
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ": " + "在线...\n"
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.Unlock()
+
+	} else {
+		this.server.Broadcast(this, msg)
+	}
 }
